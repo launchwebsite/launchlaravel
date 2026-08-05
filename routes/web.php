4a28219f71e315089190cde2833182d149782a79
launchwebsite <nikhil@ethqan.com>
2026-08-05 09:50:39 +0530

    attributes dynamic

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LogController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -84,6 +85,8 @@ Route::middleware('auth')->group(function () {
     Route::put('/admin/attributes/update/{id}', [AttributeController::class, 'update'])->name('attributes.update');
     Route::get('/admin/attributes/delete/{id}', [AttributeController::class, 'destroy'])->name('attributes.delete');
 
+
+ Route::get('/admin/product', [ProductController::class, 'index'])->name('products.index');
 });
 
 require __DIR__ . '/admin/subcategory.php';
